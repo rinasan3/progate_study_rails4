654a116d915ff3a43958a19057fe370c2177debe
Takahashi Rina <19r.takahashi90@gmail.com>
2018-03-02 09:58:42 +0900

    Add tweet_app

---
+++ tweet_app/db/.backup.sql
@@ -0,0 +1,12 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'次は何の映画をみようかな','2017-03-31 05:24:15.529063','2018-02-20 00:50:21.884083');
+INSERT INTO posts VALUES(3,replace(replace('f\r\ntest','\r',char(13)),'\n',char(10)),'2017-04-06 07:12:40.797561','2018-02-23 00:49:08.671478');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
